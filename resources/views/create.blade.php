c393875418ada91b10cdcdf16c500245c148cfbb
abhay <abhaytyagi2513@gmail.com>
2025-02-02 23:18:46 +1100

    Added form to add new tasks

---
+++ resources/views/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('title', 'Create new Task')
+
+@section('content')
+    <form method="POST" action="{{ route('tasks.store') }}">
+        @csrf
+        <div>
+            <label for="title">Title</label>
+            <input text="text" name="title" id="title" />
+        </div>
+        <div>
+            <label for="title">Description</label>
+            <textarea text="text" name="description" id="description" rows="5"></textarea>
+        </div>
+        <div>
+            <label for="title">Long Description</label>
+            <textarea text="text" name="long_description" id="long_description" rows="5"></textarea>
+        </div>
+
+        <div>
+            <button type="submit">Add Task</button>
+        </div>
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use \App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,11 +14,17 @@ Route::get('/tasks', function () {
     ]);
 })->name('tasks.index');
 
+Route::view('tasks/create', 'create')->name('tasks.create');
+
 Route::get('/tasks/{id}', function ($id) {
     return view('show', [
         'task' => Task::findOrFail($id)
     ]);
 })->name('tasks.show');
+
+Route::post('/tasks', function (Request $request) {
+    dd($request->all());
+})->name('tasks.store');
 
 Route::fallback(function () {
     return 'Not Found';
