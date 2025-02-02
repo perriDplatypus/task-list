bc5452429f6db452d00c7131e8b0eeaf65fd2336
abhay <abhaytyagi2513@gmail.com>
2025-02-02 20:16:36 +1100

    Added more code to route and make basic framework

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<h1>List of Tasks</h1>
+
+<div>
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>There are no tasks</div>
+    @endforelse
+</div>
