57722e4e796e174ca7f22a83ef975d8fa2515958
abhay <abhaytyagi2513@gmail.com>
2025-02-03 01:11:50 +1100

    Added pagination to list page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,4 +12,10 @@
             <div>There are no tasks</div>
         @endforelse
     </div>
+
+    @if ($tasks->count())
+        <nav>
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 @endsection
